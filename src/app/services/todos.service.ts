3d24ee642a4cd3ee850910143c1ec818aadff265
ManishMahale <151772671+ManishMahale@users.noreply.github.com>
2025-03-15 23:19:52 +0530

    commit

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
+  http = inject(HttpClient);
+  todoItem:Array<Todo> = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'delectus aut autem',
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'quis ut nam facilis et officia qui',
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: 'fugiat veniam minus',
+      completed: false
+    },
+    {
+      userId: 1,
+      id: 4,
+      title: 'et porro tempora',
+      completed: true
+    },
+    {
+      userId: 1,
+      id: 5,
+      title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+      completed: false
+    }
+  ];
 
-  constructor() { }
+  getTodosFromApi() {
+    const url = `https://jsonplaceholder.typicode.com/todos`;
+    return this.http.get(url);
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { catchError, of } from 'rxjs';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrls: ['./todos.component.css'], // Fixed 'styleUrl' to 'styleUrls'
 })
-export class TodosComponent {
-
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Todo[]>([]);
+    ngOnInit(): void {
+    console.log(this.todoService.getTodosFromApi());
+    this.todoItems.set(this.todoService.todoItem);
+  }
 }
